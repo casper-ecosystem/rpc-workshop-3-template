1fd7500e9a9ee0821c0ed472b1cfdb5e8aa9ffab
Dylan Ireland <dylan@casper.network>
2022-03-30 09:06:50 -0400

    Moved logic to LocalClient

--- localclient.py
+++ localclient.py
@@ -1,34 +1,38 @@
 import pycspr
 from pycspr import NodeClient
 from pycspr import NodeConnection
+from pycspr.crypto import KeyAlgorithm
 import sys
-import logic
 import pycspr.types as types
 from pycspr.types import PrivateKey
 from pycspr.types import StoredContractByHash, CL_U8, CL_ByteArray
 import time
 
 class LocalClient:
+    #-----------
+    #CONSTRUCTOR
+    #-----------
+
     def __init__(self, isHost, opponentPublicKeyHex):
         self.isHost = isHost
         self.opponentPublicKeyHex = opponentPublicKeyHex
         self.client = NodeClient(NodeConnection(host = "3.208.91.63", port_rpc = 7777))
-        self.privateKey, self.publicKey = logic.getKeys() if isHost else logic.getGuestKeys()
+        self.privateKey, self.publicKey = self.getKeys() if isHost else self.getGuestKeys()
         self.contractHash = "76dbf4629175c0141f144f690bfef42d9d24795b04dd8a3c125c0eba0158df57" #The contract hash of the TicTacToe contract deploy by Casper
         self.gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-    def setContractHashFromAccount(self, thisAccount = True, contractDeployerPublicKeyHex = None): #Change the contract to a contract owned by yourself or someone else
-        if thisAccount:
-            self.contractHash = logic.getContractHash(self.client, self.publicKey, "tictactoe_contract")
-        elif contractDeployerPublicKeyHex != None:
-            self.contractHash = logic.getContractHash(self.client, logic.publicKeyFromHex(contractDeployerPublicKeyHex), "tictactoe_contract")
-        else:
-            print("Error")
+    #-------
+    #SETTERS
+    #-------
+
+    def setContractHashFromAccount(self): #Change the contract to a contract owned by yourself or someone else
+        self.contractHash = self.getContractHash("tictactoe_contract")
+
 
     def makeMove(self, where):
         if where == "q":
             return
-        deploy = logic.makeTurnDeploy(self, where)
+        deploy = self.makeTurnDeploy(where)
         deploy.approve(self.privateKey)
         deployHash = self.client.send_deploy(deploy)
         return deployHash
@@ -36,7 +40,7 @@ class LocalClient:
 
     def setGameState(self):
         try:
-            dictState = logic.getDictionaryGameState(self)
+            dictState = self.getDictionaryGameState()
             if dictState == None:
                 print("This game is over");
                 exit(0)
@@ -49,6 +53,10 @@ class LocalClient:
             else:
                 return None
 
+    #-------
+    #GETTERS
+    #-------
+
     def hostsTurn(self):
         totalTurns = 0
         for turn in self.gameState:
@@ -60,3 +68,52 @@ class LocalClient:
 
     def getMoveDeployStatus(self, deployHash):
         return self.client.get_deploy(deployHash)
+
+    def getKeys(self):
+        privateKey = pycspr.parse_private_key("hostkeys/secret_key.pem", KeyAlgorithm.ED25519.name)
+        publicKey = pycspr.parse_public_key("hostkeys/public_key_hex")
+        return privateKey, publicKey
+
+    def getGuestKeys(self):
+        privateKey = pycspr.parse_private_key("guestkeys/secret_key.pem", KeyAlgorithm.ED25519.name)
+        publicKey = pycspr.parse_public_key("guestkeys/public_key_hex")
+        return privateKey, publicKey
+
+    def getContractHash(self, key):
+        accountInfo = client.get_account_info(self.publicKey.account_key)
+        for namedKey in accountInfo["named_keys"]:
+            if namedKey["name"] == key:
+                return namedKey["key"][5:]
+
+    def publicKeyFromHex(self, hex):
+        return types.PublicKey(KeyAlgorithm(1), bytes.fromhex(hex[2:])) #Need [2:] Because we want to ignore the Key Algorithm identifier "01"
+
+    def getDictionaryGameState(self):
+        opponentAccountHashHex = self.publicKeyFromHex(self.opponentPublicKeyHex).account_hash.hex()
+        hostHash = self.publicKey.account_hash.hex() if self.isHost else opponentAccountHashHex
+        guestHash = self.publicKey.account_hash.hex() if not self.isHost else opponentAccountHashHex
+        dictionaryID = types.DictionaryID_ContractNamedKey(dictionary_name = hostHash, dictionary_item_key = guestHash, contract_key = self.contractHash)
+        response = self.client.get_dictionary_item(dictionaryID)
+        return response.get("stored_value").get("CLValue").get("parsed")
+
+
+    #--------
+    #BUILDERS
+    #--------
+
+    def makeTurnDeploy(self, where):
+        params: DeployParameters = pycspr.create_deploy_parameters(account = self.privateKey, chain_name = "casper-test")
+        payment: ModuleBytes = pycspr.create_standard_payment(int(1e9))
+        entryPoint, opponentRole = ("host_move", "guest") if self.isHost else ("guest_move", "host") #host_move contract entry point if host, else guest_move entry point. Need to pass "host" or "guest" as player argument, because we'll need both in the contract but the contract can infer the acct hash of the caller
+
+        session = types.StoredContractByHash(
+            entry_point=entryPoint,
+            hash = bytes.fromhex(self.contractHash),
+            args = {
+                "player_move": types.CL_U8(where),
+                opponentRole: types.CL_ByteArray(self.publicKeyFromHex(self.opponentPublicKeyHex).account_hash)
+            }
+        )
+
+        deploy: Deploy = pycspr.create_deploy(params, payment, session)
+        return deploy
